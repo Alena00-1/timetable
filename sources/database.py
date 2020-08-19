942c0ca712430669d0821f44d8e06fb2ed117e7a
Alena00-1 <46621079+Alena00-1@users.noreply.github.com>
2020-08-19 08:49:59 +0300

    Update database.py

--- sources/database.py
+++ sources/database.py
@@ -1,44 +1,42 @@
 import sqlite3
 
 
+def connect_database(database):
+    connect = sqlite3.connect(database)
+    return connect
+
+
 class DataBase(object):
-    @staticmethod
-    def connect_database(database):
-        connect = sqlite3.connect(database)
-        return connect
+    def __init__(self, connect):
+        self.connect = connect
 
-    @staticmethod
-    def insert_into_subject(connect, name):
-        cur = connect.cursor()
+    def insert_into_subject(self, name):
+        cur = self.connect.cursor()
         cur.execute('''INSERT INTO subject (name_subject) VALUES(?)''', (name,))
-        connect.commit()
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def insert_into_lecturer(connect, SNP):
-        cur = connect.cursor()
+    def insert_into_lecturer(self, SNP):
+        cur = self.connect.cursor()
         cur.execute('''INSERT INTO lecturer (SNP) VALUES(?)''', (SNP,))
-        connect.commit()
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def insert_into_classroom(connect, classroom_number):
-        cur = connect.cursor()
+    def insert_into_classroom(self, classroom_number):
+        cur = self.connect.cursor()
         cur.execute('''INSERT INTO classroom (classroom_number) VALUES(?)''', (classroom_number,))
-        connect.commit()
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def insert_into_groups(connect, group_name):
-        cur = connect.cursor()
+    def insert_into_groups(self, group_name):
+        cur = self.connect.cursor()
         cur.execute('''INSERT INTO groups (group_name) VALUES(?)''', (group_name,))
-        connect.commit()
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def insert_into_timetable(connect, id_group, id_subject, id_lecturer, id_classroom, date_timetable,
+    def insert_into_timetable(self, id_group, id_subject, id_lecturer, id_classroom, date_timetable,
                               lesson_number, comment):
-        cur = connect.cursor()
+        cur = self.connect.cursor()
         cur.execute(
             '''INSERT INTO timetable 
             (id_group,
@@ -50,103 +48,97 @@ class DataBase(object):
             comment) 
             VALUES(?, ?, ?, ?, ?, ?, ?)''',
             (id_group, id_subject, id_lecturer, id_classroom, date_timetable, lesson_number, comment))
-        connect.commit()
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def delete_from_subject(connect, id):
-        cursor = connect.cursor()
+    def delete_from_subject(self, id):
+        cursor = self.connect.cursor()
         cursor.execute(
             '''DELETE FROM subject
             WHERE name_subject = ?
             ''',
             (id,))
-        connect.commit()
+        self.connect.commit()
         cursor.close()
 
-    @staticmethod
-    def delete_from_lecturer(connect, id):
-        cursor = connect.cursor()
+    def delete_from_lecturer(self, id):
+        cursor = self.connect.cursor()
         cursor.execute(
             '''DELETE FROM lecturer
             WHERE SNP = ?
             ''',
             (id,))
-        connect.commit()
+        self.connect.commit()
         cursor.close()
 
-    @staticmethod
-    def delete_from_classroom(connect, id):
-        cursor = connect.cursor()
+    def delete_from_classroom(self, id):
+        cursor = self.connect.cursor()
         cursor.execute(
             '''DELETE FROM classroom
             WHERE classroom_number = ?
             ''',
             (id,))
-        connect.commit()
+        self.connect.commit()
         cursor.close()
 
-    @staticmethod
-    def delete_from_groups(connect, id):
-        cursor = connect.cursor()
+    def delete_from_groups(self, id):
+        cursor = self.connect.cursor()
         cursor.execute(
             '''DELETE FROM groups
             WHERE group_name = ?
             ''',
             (id,))
-        connect.commit()
+        self.connect.commit()
         cursor.close()
 
-    @staticmethod
-    def delete_from_timetable(connect, date):
-        cursor = connect.cursor()
+    def delete_from_timetable(self, date):
+        cursor = self.connect.cursor()
         cursor.execute(
             '''DELETE FROM timetable
             WHERE date_timetable = ?
             ''',
             (date,))
-        connect.commit()
+        self.connect.commit()
         cursor.close()
 
-    @staticmethod
-    def create_table_subject(connect):
-        cur = connect.cursor()
+    def create_table_subject(self):
+        cur = self.connect.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS subject "
-            "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , name_subject TEXT NOT NULL);")
-        connect.commit()
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , name_subject TEXT NOT NULL, "
+            "UNIQUE ('name_subject') ON CONFLICT IGNORE);")
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def create_table_lecturer(connect):
-        cur = connect.cursor()
+    def create_table_lecturer(self):
+        cur = self.connect.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS lecturer "
-            "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , SNP TEXT NOT NULL);")
-        connect.commit()
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , SNP TEXT NOT NULL, "
+            "UNIQUE ('SNP') ON CONFLICT IGNORE);")
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def create_table_classroom(connect):
-        cur = connect.cursor()
+    def create_table_classroom(self):
+        cur = self.connect.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS classroom "
-            "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , classroom_number INTEGER NOT NULL);")
-        connect.commit()
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , classroom_number INTEGER NOT NULL, "
+            "UNIQUE ('classroom_number') ON CONFLICT IGNORE);")
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def create_table_groups(connect):
-        cur = connect.cursor()
+    def create_table_groups(self):
+        cur = self.connect.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS groups "
-            "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , group_name TEXT NOT NULL);")
-        connect.commit()
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , group_name TEXT NOT NULL, "
+            "UNIQUE ('group_name') ON CONFLICT IGNORE);")
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def create_table_timetable(connect):
-        cur = connect.cursor()
+    def create_table_timetable(self):
+        cur = self.connect.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS timetable "
             "( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , "
@@ -160,18 +152,18 @@ class DataBase(object):
             "FOREIGN KEY(ID_group) REFERENCES groups(ID),"
             "FOREIGN KEY(ID_subject) REFERENCES subject(ID),"
             "FOREIGN KEY(ID_lecturer) REFERENCES lecturer(ID),"
-            "FOREIGN KEY(ID_classroom) REFERENCES classroom(ID));")
-        connect.commit()
+            "FOREIGN KEY(ID_classroom) REFERENCES classroom(ID),"
+            "UNIQUE ('date_timetable') ON CONFLICT REPLACE);")
+        self.connect.commit()
         cur.close()
 
-    @staticmethod
-    def find_ID_classroom(connect, classroom):
-        cur = connect.cursor()
+    def find_ID_classroom(self, classroom):
+        cur = self.connect.cursor()
         cur.execute(
             '''SELECT ID FROM classroom WHERE classroom_number = ?;''',
             (classroom,))
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         if len(data) == 1:
             return data[0][0]
@@ -180,14 +172,13 @@ class DataBase(object):
         else:
             return data
 
-    @staticmethod
-    def find_ID_groups(connect, group):
-        cur = connect.cursor()
+    def find_ID_groups(self, group):
+        cur = self.connect.cursor()
         cur.execute(
             '''SELECT ID FROM groups WHERE group_name = ?;''',
             (group,))
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         if len(data) == 1:
             return data[0][0]
@@ -196,14 +187,13 @@ class DataBase(object):
         else:
             return data
 
-    @staticmethod
-    def find_ID_lecturer(connect, lecturer):
-        cur = connect.cursor()
+    def find_ID_lecturer(self, lecturer):
+        cur = self.connect.cursor()
         cur.execute(
             '''SELECT ID FROM lecturer WHERE SNP = ?;''',
             (lecturer,))
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         if len(data) == 1:
             return data[0][0]
@@ -212,14 +202,13 @@ class DataBase(object):
         else:
             return data
 
-    @staticmethod
-    def find_ID_subject(connect, subject):
-        cur = connect.cursor()
+    def find_ID_subject(self, subject):
+        cur = self.connect.cursor()
         cur.execute(
             '''SELECT ID FROM subject WHERE name_subject = ?;''',
             (subject,))
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         if len(data) == 1:
             return data[0][0]
@@ -228,9 +217,8 @@ class DataBase(object):
         else:
             return data
 
-    @staticmethod
-    def find_timetable(connect, date, group):
-        cur = connect.cursor()
+    def find_timetable(self, date, group):
+        cur = self.connect.cursor()
         cur.execute(
             '''SELECT lecturer.SNP,
                subject.name_subject,
@@ -246,42 +234,38 @@ class DataBase(object):
             (date, group)
         )
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         return data
 
-    @staticmethod
-    def find_lecturer(connect):
-        cur = connect.cursor()
+    def find_lecturer(self):
+        cur = self.connect.cursor()
         cur.execute('''SELECT SNP FROM lecturer ORDER BY SNP ASC;''')
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         return data
 
-    @staticmethod
-    def find_subject(connect):
-        cur = connect.cursor()
+    def find_subject(self):
+        cur = self.connect.cursor()
         cur.execute('''SELECT name_subject FROM subject ORDER BY name_subject ASC;''')
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         return data
 
-    @staticmethod
-    def find_classroom(connect):
-        cur = connect.cursor()
+    def find_classroom(self):
+        cur = self.connect.cursor()
         cur.execute('''SELECT classroom_number FROM classroom ORDER BY classroom_number ASC;''')
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         return data
 
-    @staticmethod
-    def find_groups(connect):
-        cur = connect.cursor()
+    def find_groups(self):
+        cur = self.connect.cursor()
         cur.execute('''SELECT group_name FROM groups ORDER BY group_name ASC;''')
         data = cur.fetchall()
-        connect.commit()
+        self.connect.commit()
         cur.close()
         return data
